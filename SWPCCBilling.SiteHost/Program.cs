192272a65915e10e54cb9f28fa2835b61d5d03ac
Andy Sherwood <xinmyname@gmail.com>
2013-08-11 11:28:23 -0700

    Renamed project, moved bower_components where it belongs

---
+++ SWPCCBilling.SiteHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using Nancy.Hosting.Self;
+
+namespace SWPCCBilling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostUrl = ConfigurationManager.AppSettings["hostUrl"];
+
+            var nancyHost = new NancyHost(new Uri(hostUrl));
+            nancyHost.Start();
+
+            Console.WriteLine("Nancy now listening - navigating to {0}. Press enter to stop", hostUrl);
+            Process.Start(hostUrl);
+            Console.ReadKey();
+
+            nancyHost.Stop();
+
+            Console.WriteLine("Stopped. Good bye!");
+        }
+    }
+}
