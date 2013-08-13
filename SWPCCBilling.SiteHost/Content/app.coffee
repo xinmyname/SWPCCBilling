0fc5aee881088f48f7482ee9a658b22af6c1a7ba
Andy Sherwood <xinmyname@gmail.com>
2013-08-12 21:38:25 -0700

    Looking good. Links and dashboard are in.

--- SWPCCBilling.SiteHost/Content/app.coffee
+++ SWPCCBilling.SiteHost/Content/app.coffee
@@ -1,3 +1,14 @@
 ﻿# CoffeeScript
 
 App = Ember.Application.create()
+
+App.Router.map ->
+    this.resource('families')
+    this.resource('fees')
+    this.resource('payments')
+    this.resource('discounts')
+    this.resource('ledger')
+    this.resource('reports')
+    this.resource('invoicing')
+    this.resource('tools')
+
--- SWPCCBilling.SiteHost/Content/app.js
+++ SWPCCBilling.SiteHost/Content/app.js
@@ -1,6 +1,17 @@
-(function() {
+﻿(function() {
   var App;
 
   App = Ember.Application.create();
+
+  App.Router.map(function() {
+    this.resource('families');
+    this.resource('fees');
+    this.resource('payments');
+    this.resource('discounts');
+    this.resource('ledger');
+    this.resource('reports');
+    this.resource('invoicing');
+    return this.resource('tools');
+  });
 
 }).call(this);
