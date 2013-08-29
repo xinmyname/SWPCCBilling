0c1dc8d3ce871b3381e99a43a7e51d01767a3db7
Andy Sherwood <xinmyname@gmail.com>
2013-08-28 20:50:47 -0700

    Getting payments to work

---
+++ SWPCCBilling.SiteHost/Models/PaymentRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SWPCCBilling.Models
+{
+    public class PaymentRequest
+    {
+        public int FamilyId { get; set; }
+        public DateTime Date { get; set; }
+        public string CheckNum { get; set; }
+        public decimal Amount { get; set; } 
+        public decimal Donation { get; set; }
+        public string Notes { get; set; }
+    }
+
+    public class PaymentReceipt
+    {
+        public decimal Amount { get; set; }
+        public string FamilyName { get; set; }
+    }
+}
